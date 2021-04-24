7e532c52df46c685b92b1df231116f130a6fe60d
武島 吉郁 (Yoshifumi Takeshima) <takeshima.y@wingarc.com>
2021-04-25 00:24:10 +0900

    refactor パッケージ移動

--- src/store/slice/App/AppSelector.ts
+++ src/store/slice/App/AppSelector.ts
@@ -1,4 +1,4 @@
-import { RootState } from "../Store";
+import { RootState } from "store/Store";
 
 export const appSelector = (state: RootState) => state.app;
 export const appHogeSelector = (state: RootState) => appSelector(state).hoge;

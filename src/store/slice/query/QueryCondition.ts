5a372600a6afb9530efc7d7aa6e6b3ee0b753a87
武島 吉郁 (Yoshifumi Takeshima) <takeshima.y@wingarc.com>
2021-04-30 19:46:50 +0900

    feat QueryConditionのsliceにnationality追加

--- src/store/slice/query/QueryCondition.ts
+++ src/store/slice/query/QueryCondition.ts
@@ -1,11 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { PlayerData } from "../../../api/data/PlayerData";
+import { PlayerData, NATIONALITY_TYPE } from "../../../api/data/PlayerData";
 
 type DataKey = keyof PlayerData;
 
 type DataValue<T extends DataKey> = PlayerData[T];
 
 export type QueryCondition = {
+  nationality: NATIONALITY_TYPE;
   axis: {
     x?: DataKey; // 横軸
     y: DataKey; // 縦軸
@@ -31,15 +32,34 @@ export const queryConditionSlice = createSlice({
   reducers: {
     addInitialQueryCondition: (
       state,
-      action: PayloadAction<{ queryConditionId: string; axis: QueryCondition["axis"] }>
+      action: PayloadAction<{ queryConditionId: string; nationality: NATIONALITY_TYPE; axis: QueryCondition["axis"] }>
     ) => {
       state.queryConditionMap[action.payload.queryConditionId] = {
+        nationality: action.payload.nationality,
         axis: action.payload.axis,
         filter: [],
       };
     },
     setQueryCondition: (state, action: PayloadAction<{ queryConditionId: string; queryCondition: QueryCondition }>) => {
       state.queryConditionMap[action.payload.queryConditionId] = action.payload.queryCondition;
+    },
+    setNationality: (state, action: PayloadAction<{ queryConditionId: string; nationality: NATIONALITY_TYPE }>) => {
+      const queryCondition = state.queryConditionMap[action.payload.queryConditionId];
+      if (queryCondition) {
+        queryCondition.nationality = action.payload.nationality;
+      }
+    },
+    setAxis: (state, action: PayloadAction<{ queryConditionId: string; axis: QueryCondition["axis"] }>) => {
+      const queryCondition = state.queryConditionMap[action.payload.queryConditionId];
+      if (queryCondition) {
+        queryCondition.axis = action.payload.axis;
+      }
+    },
+    setFilter: (state, action: PayloadAction<{ queryConditionId: string; filter: QueryCondition["filter"] }>) => {
+      const queryCondition = state.queryConditionMap[action.payload.queryConditionId];
+      if (queryCondition) {
+        queryCondition.filter = action.payload.filter;
+      }
     },
   },
 });

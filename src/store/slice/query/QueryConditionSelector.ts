0036a3833e7cf7493b063bea4d9801171e239a90
武島 吉郁 (Yoshifumi Takeshima) <takeshima.y@wingarc.com>
2021-05-01 22:56:33 +0900

    refactor keyエラー系

--- src/store/slice/query/QueryConditionSelector.ts
+++ src/store/slice/query/QueryConditionSelector.ts
@@ -4,19 +4,19 @@ export const queryConditionSelector = (queryConditionId: string) => (state: Root
   state.queryCondition.queryConditionMap[queryConditionId];
 
 export const queryConditionNationalitySelector = (queryConditionId: string) => (state: RootState) =>
-  queryConditionSelector(queryConditionId)(state)?.nationality;
+  queryConditionSelector(queryConditionId)(state)!.nationality;
 
 export const queryConditionAxisSelector = (queryConditionId: string) => (state: RootState) =>
-  queryConditionSelector(queryConditionId)(state)?.axis;
+  queryConditionSelector(queryConditionId)(state)!.axis;
 
 export const queryConditionAxisXSelector = (queryConditionId: string) => (state: RootState) =>
-  queryConditionAxisSelector(queryConditionId)(state)?.x;
+  queryConditionAxisSelector(queryConditionId)(state)!.x;
 
 export const queryConditionAxisYSelector = (queryConditionId: string) => (state: RootState) =>
-  queryConditionAxisSelector(queryConditionId)(state)?.y;
+  queryConditionAxisSelector(queryConditionId)(state)!.y;
 
 export const queryConditionAxisZSelector = (queryConditionId: string) => (state: RootState) =>
-  queryConditionAxisSelector(queryConditionId)(state)?.z;
+  queryConditionAxisSelector(queryConditionId)(state)!.z;
 
 export const queryConditionFilterSelector = (queryConditionId: string) => (state: RootState) =>
-  queryConditionSelector(queryConditionId)(state)?.filter;
+  queryConditionSelector(queryConditionId)(state)!.filter;

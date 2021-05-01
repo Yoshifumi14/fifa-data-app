c171a9eb168df863cb65653f84f494f553e67b58
武島 吉郁 (Yoshifumi Takeshima) <takeshima.y@wingarc.com>
2021-05-01 20:24:18 +0900

    fix ChartAccessor配列の変更による再レンダリング抑制

--- src/application/App.tsx
+++ src/application/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { AppLayout } from "../components/AppLayout";
-import { CreateChartDialog } from "./CreateChartDialog";
 import { useSelector, useDispatch } from "react-redux";
-import { appChartAccessorListSelector, appDrawerOpenSelector } from "../store/slice/app/AppSelector";
+import { Box } from "@material-ui/core";
+
+import { appChartAccessorListSelector, appDrawerOpenSelector } from "store/slice/app/AppSelector";
+import { setDrawerOpen } from "store/slice/app/App";
+import { ChartAccessor } from "store/slice/app/App";
+import { AppLayout } from "components/AppLayout";
+
+import { CreateChartDialog } from "./CreateChartDialog";
 import { ChartPaper } from "./ChartLogic/ChartPaper";
 import { ChartContext } from "./ChartLogic/ChartContext";
-import { setDrawerOpen } from "store/slice/app/App";
-import { Box } from "@material-ui/core";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -27,9 +29,7 @@ export const App = () => {
         {acceccerList.map((acc) => {
           return (
             <Box m={1}>
-              <ChartContext.Provider value={{ chartId: acc.chartId, queryConditionId: acc.queryConditionId }}>
-                <ChartPaper />
-              </ChartContext.Provider>
+              <ChartPaperComponent {...acc} />
             </Box>
           );
         })}
@@ -40,3 +40,9 @@ export const App = () => {
     </AppLayout>
   );
 };
+
+const ChartPaperComponent = React.memo((props: ChartAccessor) => (
+  <ChartContext.Provider value={props}>
+    <ChartPaper />
+  </ChartContext.Provider>
+));
